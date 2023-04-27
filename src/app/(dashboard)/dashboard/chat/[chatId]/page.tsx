52a14294ee7154979f44281a3543b37e3380709b
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2023-04-27 18:01:25 -0300

    fixed final server errors

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,11 +1,12 @@
-import ChatInput from '@/components/ChatInput'
-import Messages from '@/components/Messages'
-import { fetchRedis } from '@/helpers/redis'
-import { authOptions } from '@/lib/auth'
-import { messageArrayValidator } from '@/lib/validations/message'
-import { getServerSession } from 'next-auth'
-import Image from 'next/image'
-import { notFound } from 'next/navigation'
+import ChatInput from "@/components/ChatInput"
+import Messages from "@/components/Messages"
+import { fetchRedis } from "@/helpers/redis"
+import { authOptions } from "@/lib/auth"
+import { messageArrayValidator } from "@/lib/validations/message"
+import { getServerSession } from "next-auth"
+import Head from "next/head"
+import Image from "next/image"
+import { notFound } from "next/navigation"
 
 // The following generateMetadata functiion was written after the video and is purely optional
 export async function generateMetadata({
@@ -15,12 +16,12 @@ export async function generateMetadata({
 }) {
   const session = await getServerSession(authOptions)
   if (!session) notFound()
-  const [userId1, userId2] = params.chatId.split('--')
+  const [userId1, userId2] = params.chatId.split("--")
   const { user } = session
 
   const chatPartnerId = user.id === userId1 ? userId2 : userId1
   const chatPartnerRaw = (await fetchRedis(
-    'get',
+    "get",
     `user:${chatPartnerId}`
   )) as string
   const chatPartner = JSON.parse(chatPartnerRaw) as User
@@ -37,7 +38,7 @@ interface PageProps {
 async function getChatMessages(chatId: string) {
   try {
     const results: string[] = await fetchRedis(
-      'zrange',
+      "zrange",
       `chat:${chatId}:messages`,
       0,
       -1
@@ -62,7 +63,7 @@ const page = async ({ params }: PageProps) => {
 
   const { user } = session
 
-  const [userId1, userId2] = chatId.split('--')
+  const [userId1, userId2] = chatId.split("--")
 
   if (user.id !== userId1 && user.id !== userId2) {
     notFound()
@@ -72,49 +73,54 @@ const page = async ({ params }: PageProps) => {
   // new
 
   const chatPartnerRaw = (await fetchRedis(
-    'get',
+    "get",
     `user:${chatPartnerId}`
   )) as string
   const chatPartner = JSON.parse(chatPartnerRaw) as User
   const initialMessages = await getChatMessages(chatId)
 
   return (
-    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
-      <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
-        <div className='relative flex items-center space-x-4'>
-          <div className='relative'>
-            <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
-              <Image
-                fill
-                referrerPolicy='no-referrer'
-                src={chatPartner.image}
-                alt={`${chatPartner.name} profile picture`}
-                className='rounded-full'
-              />
-            </div>
-          </div>
-
-          <div className='flex flex-col leading-tight'>
-            <div className='text-xl flex items-center'>
-              <span className='text-gray-700 mr-3 font-semibold'>
-                {chatPartner.name}
-              </span>
+    <>
+      <Head>
+        <title>Dashboard | Chat</title>
+      </Head>
+      <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]">
+        <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+          <div className="relative flex items-center space-x-4">
+            <div className="relative">
+              <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+                <Image
+                  fill
+                  referrerPolicy="no-referrer"
+                  src={chatPartner.image}
+                  alt={`${chatPartner.name} profile picture`}
+                  className="rounded-full"
+                />
+              </div>
             </div>
 
-            <span className='text-sm text-gray-600'>{chatPartner.email}</span>
+            <div className="flex flex-col leading-tight">
+              <div className="text-xl flex items-center">
+                <span className="text-gray-700 mr-3 font-semibold">
+                  {chatPartner.name}
+                </span>
+              </div>
+
+              <span className="text-sm text-gray-600">{chatPartner.email}</span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <Messages
-        chatId={chatId}
-        chatPartner={chatPartner}
-        sessionImg={session.user.image}
-        sessionId={session.user.id}
-        initialMessages={initialMessages}
-      />
-      <ChatInput chatId={chatId} chatPartner={chatPartner} />
-    </div>
+        <Messages
+          chatId={chatId}
+          chatPartner={chatPartner}
+          sessionImg={session.user.image}
+          sessionId={session.user.id}
+          initialMessages={initialMessages}
+        />
+        <ChatInput chatId={chatId} chatPartner={chatPartner} />
+      </div>
+    </>
   )
 }
 
